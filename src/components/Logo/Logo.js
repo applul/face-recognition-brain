f7be416051e5c4057993400850c61229a7d0c09e
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-23 10:47:59 +0800

    I'm committed.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tachyons from 'tachyons';
+import Tilt from 'react-tilt';
+
+const Logo =() => {
+    return(
+        <div className = 'ma4  mt0 '>
+            <p>Hello Holo</p>
+        </div>
+    )
+}
+
+
+export default Logo;
