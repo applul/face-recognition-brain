2b372354f81b0c06671d5ebfd69e00d0bb3258f2
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-25 09:29:27 +0800

    I'm committed.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Tachyons from 'tachyons';
 import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
 
 const Logo =() => {
     return(
         <div className = 'ma4  mt0 '>
             <p>Hello Holo</p>
+            <Tilt className="Tilt b2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3 ">
+                    <img id="logo" alt = "Holo's Logo" src={brain}/>
+                </div>
+                <brain />
+            </Tilt>
         </div>
     )
 }
