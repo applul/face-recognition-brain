7195a42f0a65564c160c5a7dc2d9442704068e14
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-26 22:18:11 +0800

    miscellaneous changes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,7 @@ import brain from './brain.png';
 
 const Logo =() => {
     return(
-        <div className = 'ma4  mt0 '>
-            <p>Hello Holo</p>
+        <div className = 'ma4  mt0 '> 
             <Tilt className="Tilt b2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa3 ">
                     <img id="logo" alt = "Holo's Logo" src={brain}/>
