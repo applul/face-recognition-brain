4ca023628840cf997c5011f162526d6dc32aa8ab
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-26 22:47:12 +0800

    miscellaneous changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  line_linked: {
+    shadow: {
+      enable: true,
+      color: "#3CA9D1",
+      blur: 5
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles 
+        params={particlesOptions}
+      />    
     <Navigation />
     <Logo />
     <Rank />
