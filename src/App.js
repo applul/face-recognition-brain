ce0a69aa53a8dc184111a21cc53377a48c086804
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-30 18:32:25 +0800

    removing some comments and doing spring cleaning

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
     })
     app.models.predict( Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         this.calculateFaceLocation(response);
         // do something with response
       },
