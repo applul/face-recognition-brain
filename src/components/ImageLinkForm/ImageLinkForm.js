df5e013e151db601602ef557447536c81a578d08
applul <ray.houston44@gmail.com>
2019-06-04 17:24:56 +0800

    added disclaimer

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,8 @@ import Tachyons from 'tachyons';
 const ImageLinkForm =({ onInputChange, onSubmit }) => {
     return(
         <div>
-            <p className = 'f3'>This magic brain application will detect faces. Give it a try!</p>
+            <p className = 'f3'>This magic brain application will detect faces. Give it a try!</p> <br></br>
+            <p id="disclaimer">(Only one face per image)</p>
             <div className ="center">
                 <div className = " form center pa4 br3 shadow-5">
                     <input className='f4 pa2 w-70 center' placeholder='Enter url here' type='text' onChange = {onInputChange} />
